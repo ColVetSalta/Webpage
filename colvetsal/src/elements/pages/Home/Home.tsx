6919690089412a349ae25a42ce17dbc7b4c816ae
Alba Tyto <gperettig@gmail.com>
2023-08-30 08:22:42 -0300

    add (assets&Home) add some background pics

--- colvetsal/src/elements/pages/Home/Home.tsx
+++ colvetsal/src/elements/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Image } from '@chakra-ui/react';
+import TopBack from '../../../assets/DSCN2717.png'
 interface HomeProps {}
 
 const Home: React.FC <HomeProps> = ()=> {
 	return (
 	<>		
+	<Image width={'99dvw'} src={TopBack} alt="top background"/>
 	<h1>Bienvenidos</h1>
 	</>		
 	)
