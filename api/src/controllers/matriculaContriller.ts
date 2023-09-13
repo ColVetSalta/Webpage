1a8365802cbf7bec408a40cff5a862ea1212607b
Alba Tyto <gperettig@gmail.com>
2023-09-13 19:08:26 -0300

    feat(controllers empty files)

---
+++ api/src/controllers/matriculaContriller.ts
@@ -0,0 +1,32 @@
+// /* eslint-disable @typescript-eslint/naming-convention */
+// import Marticulado from '../db'
+
+// interface Matriculado {
+//   mp: number
+//   nombre: string
+//   apellido: string
+//   telefono: string
+//   correo_electronico: string
+//   f_nacimiento: string
+//   direccion: string
+// }
+
+// export async function postMatriculado ({
+//   mp,
+//   nombre,
+//   apellido,
+//   telefono,
+//   correo_electronico,
+//   f_nacimiento,
+//   direccion
+// }: Matriculado): Promise<any> {
+//   return await Marticulado.create({
+//     mp,
+//     nombre,
+//     apellido,
+//     telefono,
+//     correo_electronico,
+//     f_nacimiento,
+//     direccion
+//   })
+// }
