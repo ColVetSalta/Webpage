4e205be28c98705755d224b1118cb9efcad029af
Alba Tyto <gperettig@gmail.com>
2023-09-16 12:17:12 -0300

    feat(/api/src): Periodo complete basic controller

--- api/src/controllers/periodoController.ts
+++ api/src/controllers/periodoController.ts
@@ -17,20 +17,23 @@ export async function postPeriodo ({
   })
 }
 
-/* export async function getPeriodos (): Promise<Periodo[]> {
+export async function getPeriodos (): Promise<Periodo[]> {
   return await Periodo.findAll()
 }
-export async function getPeriodo (id: number): Promise<any> {
+
+export async function getPeriodoById (id: number): Promise<any> {
   const charge = await Periodo.findOne({
     where: { id }
   })
   return charge?.toJSON()
 }
-*/
-/* export async function editPeriodo (data: Periodo): Promise<[affectedCount: number]> {
+
+export async function editPeriodo (data: Periodo): Promise<[affectedCount: number]> {
   return await Periodo.update({
-    nombre: data.nombre,
-    orgId: data.orgId
+    fecha_inicio: data.fecha_inicio,
+    fecha_final: data.fecha_final,
+    mp: data.mp,
+    cargoId: data.cargoId
   },
   {
     where: {
@@ -38,10 +41,9 @@ export async function getPeriodo (id: number): Promise<any> {
     }
   })
 }
-*/
-/* export async function deletePeriodo (id: number): Promise<string> {
+
+export async function deletePeriodo (id: number): Promise<string> {
   await Periodo.destroy({ where: { id } })
     .then(() => console.log(`Periodo ${id} eliminado`))
   return `Periodo ${id} eliminado`
 }
-*/
