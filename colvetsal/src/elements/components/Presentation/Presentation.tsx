324f0b8caab43c2632bf985b28a34fedd1ef83e8
Alba Tyto <gperettig@gmail.com>
2023-11-08 10:24:47 -0300

    feat(colvetsal/Presentation & RoleData) discriminate cases of presentation -motive-

--- colvetsal/src/elements/components/Presentation/Presentation.tsx
+++ colvetsal/src/elements/components/Presentation/Presentation.tsx
@@ -2,21 +2,37 @@ import p from './Presentation.module.css'
 // import { Grid, GridItem, Heading } from '@chakra-ui/react'
 import RoleData from './RoleData';
 import { useAppSelector } from '../../../redux/hooks'
-export default function Presentation({ motive }: { motive: string }): JSX.Element {
-    const { organism } = useAppSelector((state)=>state.org)
-    const subind: string = motive.split('_')[1].toUpperCase();
-            const divitions: string[] = Object.keys(organism);
+import { Organismo } from '../../../types';
 
-            return <div className={p.cont}>
-                        <h3>{subind}</h3>
-                {
-                    divitions ? divitions.map((d) => {
-                        const h = organism[d as keyof typeof organism]                  
-                        return <div>
-                            <h3>{d}</h3>
-                            {h ? h.map((r)=> <RoleData r={r} />) : null}                            
-                        </div>
-                    }) : null
-                }
-            </div>
+export interface DefPres {
+    [x: string]: [{
+        pendiente: string
+    }
+]}
+
+export default function Presentation({ motive }: { motive: string }): JSX.Element {
+
+    const subind: string = motive.split('_')[1].toUpperCase();
+    const { organism } = useAppSelector((state) => state.org)
+    let stateSelect: Organismo | DefPres = {'PROXIMAMENTE': [{
+        pendiente: 'Estamos trabajando para presentar este espacio en un futuro'
+    }]}
+
+    if (subind === 'AUTORIDADES') stateSelect = organism
+
+    const divitions: string[] = Object.keys(stateSelect);
+
+    return <div className={p.cont}>
+        <h3>{subind}</h3>
+        <h3>Consejo Mayor: </h3>
+        {
+            divitions ? divitions.map((d) => {
+                const h = stateSelect[d as keyof typeof stateSelect]
+                return <div>
+                    <h3>{d}</h3>
+                    {h ? h.map((r) => <RoleData r={r} />) : null}
+                </div>
+            }) : null
+        }
+    </div>
 }
--- colvetsal/src/elements/components/Presentation/RoleData.tsx
+++ colvetsal/src/elements/components/Presentation/RoleData.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { type Role } from '../../../types'
 
-export default function RoleData ({ r }: {r: Role}): React.JSX.Element {
-    console.log(r)
-    return <div>
+export default function RoleData({ r }: { r: Role | { pendiente: string } }): React.JSX.Element {
+    if ('cargo' in r) {
+        return <div>
             <h4>{r.cargo}</h4>
-            <h5>{r.nombre+' '+r.apellido}</h5>
+            <h5>{r.nombre + ' ' + r.apellido}</h5>
             <h6>Telefono: {r.numero}</h6>
-            <h6>e-mail: {r.correo_electronico}</h6>         
+            <h6>e-mail: {r.correo_electronico}</h6>
         </div>
+    } else if ('pendiente' in r) {
+        return <div>
+            <h3>{r.pendiente}</h3>
+        </div>
+    }
+    return <div></div>
 }
