10f838ea36ad308fb2e79b57dd3b26bb847e45d1
Alba Tyto <gperettig@gmail.com>
2023-12-13 19:36:59 -0300

    feat(PostMatriculadoForm): validate registration Date

--- colvetsal/src/utils/ValMatriculado.ts
+++ colvetsal/src/utils/ValMatriculado.ts
@@ -111,34 +111,42 @@ export function ValMatriculado({ input, setError }: IValMatriculado) {
     setError((prevError) => ({ ...prevError, departamento_d_laboral: "pass" }));
   }
   if (input.f_nacimiento && new Date(input.f_nacimiento) instanceof Date) {
-    console.log('fecha de nacimiento')    
     const inputDate = Date.parse(new Date(input.f_nacimiento).toDateString())
     const maxDate = Date.now() - 631152000000
-    const minDate = Date.parse('01/01/1988') - 2048716800000
-  console.log('variables : ',minDate, ' - ', inputDate, ' - ', maxDate)    
-  console.log(maxDate <= inputDate || inputDate <= minDate) 
+    const minDate = Date.parse('01/01/1988') - 2051244000000
     if(maxDate <= inputDate || inputDate <= minDate) {
     setError((prevError) => ({ ...prevError, f_nacimiento: "Fecha fuera de rango válido, revise la Fecha de Nacimiento" }));
     } else {
     setError((prevError) => ({ ...prevError, f_nacimiento: "pass" }))
     }
   }
-  if (input.f_alta  && input.f_alta instanceof Date) {
-    setError((prevError) => ({ ...prevError, f_alta: "pass" }));
+  if (input.f_alta  && new Date(input.f_alta) instanceof Date) {    
+    const inputDate = Date.parse(new Date(input.f_alta).toDateString())
+    const maxDate = Date.now()
+    const minDate = Date.parse('01/01/1988')
+    if(maxDate <= inputDate || inputDate <= minDate) {
+      setError((prevError) => ({ ...prevError, f_alta: "Fecha fuera de rango válido, revise la Fecha de Alta" }));
+    } else {
+    setError((prevError) => ({ ...prevError, f_alta: "pass" }))
+    }
   }
+  console.log(input.telefono.length)  
   if (input.telefono.length === 0 || input.telefono[0].numero === "") {
+    console.log('telefono no tiene registros: ', input.telefono[0].numero === "")    
     setError((prevError) => ({
       ...prevError,
       telefono: "Debe ingresar al menos un número telefónico",
     }));
   } else if (input.telefono.length === 1) {
+    console.log('telefono tiene un solo registro: ', input.telefono.length === 1)
     if (input.telefono[0].principal) {
       setError((prevError) => ({
         ...prevError,
         telefono: "pass",
       }));
     } else {
-      setError((prevError) => ({
+    console.log('telefono tiene varios registros: ', input.telefono.length > 1)
+    setError((prevError) => ({
         ...prevError,
         telefono:
           "Debe tener un telefono principal, no olvide marcar la opción",
@@ -169,6 +177,13 @@ export function ValMatriculado({ input, setError }: IValMatriculado) {
             "Debe tener un telefono principal, no olvide marcar la opción",
         };
       });
+    } else {
+      setError((prevError) => {
+        return {
+          ...prevError,
+          telefono: 'pass'
+        }
+      })
     }
   }
 }
